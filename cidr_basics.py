0e880a39e96844c9061dc1c8a35b88c58a6fa7d2
miguel-mancera997 <113144211+miguel-mancera997@users.noreply.github.com>
2026-01-21 18:37:47 -0800

    network/broadcast addressing

--- cidr_basics.py
+++ cidr_basics.py
@@ -30,16 +30,48 @@ def ip_to_bits(ip):
 def format_bits(bits):
     return bits[0:8] + " " + bits[8:16] + " " + bits[16:24] + " " + bits[24:32]
 
+def bits_to_ip(bits32):
+    octets = []
+    for i in range(0, 32, 8):
+        chunk = bits32[i:i+8]
+        octets.append(str(int(chunk, 2)))
+    return ".".join(octets)
+
 
 def apply_cidr(bits32, cidr):
     network_bits = bits32[:cidr]
     host_bits = bits32[cidr:]
     return network_bits, host_bits
 
-bits32 = ip_to_bits("192.168.1.50")
-network, host = apply_cidr(bits32, 24)
+def network_address(bits32, cidr):
+    net = bits32[:cidr]
+    host_len = 32 - cidr
+    return net + ("0" * host_len)
+
+def broadcast_address(bits32, cidr):
+    net = bits32[:cidr]
+    host_len = 32 - cidr
+    return net + ("1" * host_len)
+
+
+ip = "192.168.1.50"
+cidr = 24
+
+
+bits32 = ip_to_bits(ip)
+net_bits, host_bits = apply_cidr(bits32, cidr)
+
+net_addr_bits = network_address(bits32, cidr)
+bcast_bits = broadcast_address(bits32, cidr)
+
+print("IP:", ip)
+print("CIDR: /" + str(cidr))
 print("Full 32 bits:", format_bits(bits32))
-print("Network bits:", network)
-print("Host bits:", host)
-print("Network bit count:", len(network))
-print("Host bit count:", len(host))
+print("Network bits:", net_bits)
+print("Host bits:", host_bits)
+
+print("Network address bits:", format_bits(net_addr_bits))
+print("Network address:", bits_to_ip(net_addr_bits))
+
+print("Broadcast bits:", format_bits(bcast_bits))
+print("Broadcast address:", bits_to_ip(bcast_bits))
